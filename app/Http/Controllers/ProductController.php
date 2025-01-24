abac9576d256a9f559bd2c5a6fad81f00b9760bb
kasurbantal <kasurbantal880@gmail.com>
2025-01-24 21:15:57 +0700

    feat: custom validate alert

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,10 +9,13 @@ use App\Models\Product;
 use Illuminate\View\View;
 
 //import return type redirectResponse
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 //import Http Request
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+
+//import Facades Storage
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -24,7 +27,7 @@ class ProductController extends Controller
     public function index() : View
     {
         //get all products
-        $products = Product::latest()->paginate(5);
+        $products = Product::latest()->paginate(10);
 
         //render view with products
         return view('products.index', compact('products'));
@@ -55,7 +58,11 @@ class ProductController extends Controller
             'description'   => 'required|min:10',
             'price'         => 'required|numeric',
             'stock'         => 'required|numeric'
-        ]);
+        ], ['image.required' => 'isi bro',
+            'image.image' => 'image dong bro',
+            'image.mimes' => 'yang sesuai dong bro',
+            'image.max' => 'maxnya 2 mb bro',
+            'title.min' => 'minimal 5 suku kata mas']);
 
         //upload image
         $image = $request->file('image');
@@ -74,7 +81,7 @@ class ProductController extends Controller
         return redirect()->route('products.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 
-     /**
+    /**
      * show
      *
      * @param  mixed $id
@@ -87,5 +94,75 @@ class ProductController extends Controller
 
         //render view with product
         return view('products.show', compact('product'));
+    }
+
+    /**
+     * edit
+     *
+     * @param  mixed $id
+     * @return View
+     */
+    public function edit(string $id): View
+    {
+        //get product by ID
+        $product = Product::findOrFail($id);
+
+        //render view with product
+        return view('products.edit', compact('product'));
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $id
+     * @return RedirectResponse
+     */
+    public function update(Request $request, $id): RedirectResponse
+    {
+        //validate form
+        $request->validate([
+            'image'         => 'image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        //get product by ID
+        $product = Product::findOrFail($id);
+
+        //check if image is uploaded
+        if ($request->hasFile('image')) {
+
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/products', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/products/'.$product->image);
+
+            //update product with new image
+            $product->update([
+                'image'         => $image->hashName(),
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+
+        } else {
+
+            //update product without image
+            $product->update([
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+        }
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Diubah!']);
     }
 }
